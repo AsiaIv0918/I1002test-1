eca092622c1e3a785dabef7f76d44e914231b47d
AsiaIv0918 <104051067@live.asia.edu.tw>
2017-10-02 15:12:53 +0800

    NEW

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.*;
 public class Main {
-
+//字串字元置換
     public static void main(String[] args) {
-	// write your code here
+	Scanner in=new Scanner(System.in);
+	String str;
+	char a,b;
+    str=in.nextLine();
+    a=in.next().charAt(0);
+    b=in.next().charAt(0);
+
+    System.out.println(str.replace(a,b));
     }
 }
